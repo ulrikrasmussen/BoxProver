736af2ad873c5fd48bd217520609182cc9164e6f
Ulrik Rasmussen <ulrik.rasmussen@deondigital.com>
2022-06-12 12:52:52 +0200

    start.sh: log into /log subdir

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "var SERVER_URL = '$server_url'" > /opt/boxprover/frontend/config.js
 /opt/boxprover/boxprover \
       --twelf-server=/opt/boxprover/twelf-server \
       --base-sig=/opt/boxprover/data/fitch.elf \
-      --check-log=/opt/boxprover/check.log \
-      --access-log=/opt/boxprover/access.log \
-      --error-log=/opt/boxprover/error.log \
+      --check-log=/opt/boxprover/log/check.log \
+      --access-log=/opt/boxprover/log/access.log \
+      --error-log=/opt/boxprover/log/error.log \
       -p 8000
